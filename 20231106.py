42695264321086e692a2e24fcd2de196860d387e
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-11-06 11:48:04 +0100

    Create 20231106.py

---
+++ 20231106.py
@@ -0,0 +1,21 @@
+x=print("hello")
+print(x)
+
+def fv(num):
+    if(num>5):
+        return 0
+    else:
+        print("kisebb")
+
+y=fv(3)
+print(y)
+
+print("hello\nWo\trld")
+print(len("\n"))
+
+print("hdfsk\\number")
+
+print("elsoszo", "masodikszo", end=" ")
+print("Python")
+
+print("elsoszo", "masodikszo", sep=":")
