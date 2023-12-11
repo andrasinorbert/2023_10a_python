85121f556279b0ae1ce0894b828cad3349176986
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-12-11 11:03:19 +0100

    Update main.py

---
+++ 20231211/main.py
@@ -0,0 +1,9 @@
+# Adatok:
+#   Fájlban:    Dolgozók nevei szerepelnek
+#               Mellette hogy havonta mennyi palacsintát sütöttek meg
+
+# 1) Olvasd be a fájlt megfelelő adatszerkezetbe!
+
+# 2) írd ki az adatokat szépen(!!) az alábbbi fejléccel:
+#       "Név\tJanuary\tFebruary\tMarch\tApril\tMay\tJune\tJuly\tAugust\tSeptember\tOctober\tNovember\tDecember"
+
