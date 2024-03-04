95e2c071b821d8952771ccf03cced2872d688dc3
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2024-03-04 10:16:04 +0100

    20240304_dolgozat

---
+++ 20240304_dolgozat/dolgozat.py
@@ -0,0 +1,32 @@
+"""
+1. Fájl átnevezése: osztály_név.py
+
+Feladatok:
+1. A beolvas fv-ben valósítsd meg, hogy a paraméterben megadott fájlt beolvassa, és térjen vissza a szám listával az átalakítások után
+2. A feladat1,2 stb fv-ben valósítsd meg a kért eljárásokat! A fájl beolvasásához használd a beolvas() függvényedet!
+3. feladat1(): számold ki a fájlokban található számok összegét a tanult prog tétel segítségével!
+4. feladat2(): számold ki a fájlokban található számok átlagát a tanult prog tétel segítségével!
+5. feladat3(): számold ki a fájlokban található számok minimumát a tanult prog tétel segítségével!
+6. feladat4(): számold ki a fájlokban található számok maximumát a tanult prog tétel segítségével!
+
+Input fájlok használata:
+feladat1() -> input1
+feladat2() -> input2
+feladat3() -> input3
+feladat4() -> input3
+"""
+def beolvas():
+    pass
+def feladat1():
+    pass
+def feladat2():
+    pass
+def feladat3():
+    pass
+def feladat4():
+    pass
+
+feladat1()
+feladat2()
+feladat3()
+feladat4()
