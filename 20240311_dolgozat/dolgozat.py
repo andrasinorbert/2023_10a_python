989a11f70493d46fb99e27e704c54b1571626e4d
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2024-03-11 10:06:55 +0100

    20240311 dolgozat

---
+++ 20240311_dolgozat/dolgozat.py
@@ -0,0 +1,36 @@
+"""
+1. Fájl átnevezése: osztály_név.py
+
+Feladatok:
+1. A beolvas fv-ben valósítsd meg, hogy a paraméterben megadott fájlt beolvassa, és térjen vissza a szám listával az átalakítások után
+2. A feladat1,2 stb fv-ben valósítsd meg a kért eljárásokat! A fájl beolvasásához használd a beolvas() függvényedet!
+3. feladat1(): számold ki a fájlban található számok összegét a tanult prog tétel segítségével!
+4. feladat2(): számold ki a fájlban található számok átlagát a tanult prog tétel segítségével!
+5. feladat3(): számold ki a fájlban található számok minimumát a tanult prog tétel segítségével!
+6. feladat4(): számold ki a fájlban található számok maximumát a tanult prog tétel segítségével!
+
+Kiírások:
+feladat1: [eredmény]
+feladat2: [eredmény]
+feladat3: [eredmény]
+feladat4: [eredmény]
+A ": " után csak az eremény szerepeljen
+
+A dolgozat python fájlját küldd el a andrasi.norbert@puskas.hu-ra az alábbi tárggyal:
+[osztály][filebeolvasas2] név 
+"""
+def beolvas():
+    pass
+def feladat1():
+    pass
+def feladat2():
+    pass
+def feladat3():
+    pass
+def feladat4():
+    pass
+
+feladat1()
+feladat2()
+feladat3()
+feladat4()
